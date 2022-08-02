73fdc6f324d4d369979ad81656b715e48ccd336f
Nika Perfilova <ees2009.2014@gmail.com>
2022-08-02 20:32:53 +0700

    feat: add buttons and style for car view

--- async-race/src/components/car-view.ts
+++ async-race/src/components/car-view.ts
@@ -1,5 +1,7 @@
 import Car from '../types/car';
 
+const SVG_PATH = 'spritemap.svg#sprite-car';
+
 class CarView {
   container: HTMLElement;
 
@@ -18,11 +20,23 @@ class CarView {
 
   create(): HTMLElement {
     this.container.setAttribute('data-car-id', this.id.toString());
-    this.container.innerHTML = `<h2>${this.name}</h2>
-    <div>
-      <svg fill="${this.color}" class="car-svg">
-        <use xlink:href="spritemap.svg#sprite-car"></use>
+    this.container.classList.add('car', 'border-bottom', 'p-4', 'vstack', 'gap-3');
+    this.container.innerHTML = `<div class="hstack gap-3 py-2 border-bottom">
+      <h2 class="h3 me-auto">${this.name}</h2>
+      <button class="btn btn-outline-primary btn-sm">Edit</button>
+      <div class="vr"></div>
+      <button class="btn btn-danger btn-sm">Delete</button>
+    </div>
+
+    <div class="car__track px-3 pb-0 border-bottom border-3">
+      <svg fill="${this.color}" class="car__icon position-relative" viewBox="0 0 1280 640" fill="lightgreen">
+        <use xlink:href="${SVG_PATH}"></use>
       </svg>
+    </div>
+
+    <div class="btn-group me-auto">
+      <button class="btn btn-primary btn-sm">Start</button>
+      <button class="btn btn-secondary btn-sm">Stop</button>
     </div>`;
 
     return this.container;
